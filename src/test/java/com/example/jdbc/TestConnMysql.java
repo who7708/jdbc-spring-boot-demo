97fa99fb5ab9ee3e05952fb962e9de9bad959a04
Chris <who7708@163.com>
2023-08-26 22:17:50 +0800

    增加单元测试junit

--- src/test/java/com/example/jdbc/TestConnMysql.java
+++ src/test/java/com/example/jdbc/TestConnMysql.java
@@ -2,7 +2,12 @@ package com.example.jdbc;
 
 import com.mysql.jdbc.Driver;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -33,5 +38,20 @@ public class TestConnMysql {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    @Test
+    public void test2() throws Exception {
+        System.out.println("===== test2 =====");
+        Method element = CustomerServiceImpl.class.getMethod("insertSelective", Customer.class);
+        // element.getAnnotations()
+        // element.getDeclaredAnnotations()
+        // 5.x+ 调用
+        AnnotationAttributes attributes = AnnotatedElementUtils.findMergedAnnotationAttributes(element, Transactional.class, false, false);
+        System.out.println(attributes);
+
+        // 4.x- 调用
+        AnnotationAttributes attributes2 = AnnotatedElementUtils.getMergedAnnotationAttributes(element, Transactional.class);
+        System.out.println(attributes2);
     }
 }
